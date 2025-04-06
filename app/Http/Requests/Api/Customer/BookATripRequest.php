d807cad293f2d12e542522f6282588994e54f6fe
PrinceBhatt07 <bhattprince50@gmail.com>
2025-04-06 22:40:47 +0530

    Fix Issue

--- app/Http/Requests/Api/Customer/BookATripRequest.php
+++ app/Http/Requests/Api/Customer/BookATripRequest.php
@@ -74,4 +74,15 @@ class BookATripRequest extends FormRequest
         ];
     }
 
+
+    // For ProhibitedIf rule
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            if ($this->is_new_car_promised == true && $this->is_diesel_car_needed == true) {
+                $validator->errors()->add('is_diesel_car_needed', 'Due to the new government policy, a new car cannot be a diesel car. Please select either a new car or a diesel car, but not both.');
+            }
+        });
+    }
+
 }
